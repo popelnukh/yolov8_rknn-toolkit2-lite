2040912b1da77441e30924fbd1ace557163df10a
orangepi <popelnukh@gmail.com>
2024-05-22 17:45:15 +0300

    Commit of yolov8_lite_video.py

---
+++ yolov8_lite_video.py
@@ -0,0 +1,313 @@
+import os
+import cv2
+import sys
+import time
+import argparse
+from rknnlite.api import RKNNLite
+from scipy.special import softmax
+
+
+from coco_utils import COCO_test_helper
+import numpy as np
+
+OBJ_THRESH = 0.25
+NMS_THRESH = 0.45
+
+
+IMG_SIZE = (640, 640)  # (width, height), such as (1280, 736)
+#IMG_SIZE = (320, 320)  # (width, height), such as (1280, 736)
+fps = None
+
+# CLASSES = ('tank', 'btr', 'bmp', 'soldier', 'corpse')
+CLASSES = ('tank', 'btr', 'btr broken', 'bmp', 'bmp broken', 'mlrs grad', 'army truck', 'soldier', 'corpse', 'z', 'hatch')
+#CLASSES = ('BMP-BMD', 'BTR', 'KAMAZ', 'MTLB', 'RLS', 'RSZO', 'SAU', 'TANK', 'URAL', 'ZRK')
+
+# CLASSES = ("person", "bicycle", "car", "motorbike ", "aeroplane ", "bus ", "train", "truck ", "boat", "traffic light",
+#            "fire hydrant", "stop sign ", "parking meter", "bench", "bird", "cat", "dog ", "horse ", "sheep", "cow",
+#            "elephant",
+#            "bear", "zebra ", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis",
+#            "snowboard", "sports ball", "kite",
+#            "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+#            "fork", "knife ",
+#            "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza ", "donut",
+#            "cake", "chair", "sofa",
+#            "pottedplant", "bed", "diningtable", "toilet ", "tvmonitor", "laptop	", "mouse	", "remote ",
+#            "keyboard ", "cell phone", "microwave ",
+#            "oven ", "toaster", "sink", "refrigerator ", "book", "clock", "vase", "scissors ", "teddy bear ",
+#            "hair drier", "toothbrush ")
+
+coco_id_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 27, 28, 31, 32,
+                33, 34,
+                35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61,
+                62, 63,
+                64, 65, 67, 70, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 84, 85, 86, 87, 88, 89, 90]
+
+
+def filter_boxes(boxes, box_confidences, box_class_probs):
+    """Filter boxes with object threshold.
+    """
+    box_confidences = box_confidences.reshape(-1)
+    candidate, class_num = box_class_probs.shape
+
+    class_max_score = np.max(box_class_probs, axis=-1)
+    classes = np.argmax(box_class_probs, axis=-1)
+
+    _class_pos = np.where(class_max_score * box_confidences >= OBJ_THRESH)
+    scores = (class_max_score * box_confidences)[_class_pos]
+
+    boxes = boxes[_class_pos]
+    classes = classes[_class_pos]
+
+    return boxes, classes, scores
+
+
+def nms_boxes(boxes, scores):
+    """Suppress non-maximal boxes.
+    # Returns
+        keep: ndarray, index of effective boxes.
+    """
+    x = boxes[:, 0]
+    y = boxes[:, 1]
+    w = boxes[:, 2] - boxes[:, 0]
+    h = boxes[:, 3] - boxes[:, 1]
+
+    areas = w * h
+    order = scores.argsort()[::-1]
+
+    keep = []
+    while order.size > 0:
+        i = order[0]
+        keep.append(i)
+
+        xx1 = np.maximum(x[i], x[order[1:]])
+        yy1 = np.maximum(y[i], y[order[1:]])
+        xx2 = np.minimum(x[i] + w[i], x[order[1:]] + w[order[1:]])
+        yy2 = np.minimum(y[i] + h[i], y[order[1:]] + h[order[1:]])
+
+        w1 = np.maximum(0.0, xx2 - xx1 + 0.00001)
+        h1 = np.maximum(0.0, yy2 - yy1 + 0.00001)
+        inter = w1 * h1
+
+        ovr = inter / (areas[i] + areas[order[1:]] - inter)
+        inds = np.where(ovr <= NMS_THRESH)[0]
+        order = order[inds + 1]
+    keep = np.array(keep)
+    return keep
+
+
+def dfl(position):
+    x = np.array(position)
+    n, c, h, w = x.shape
+    p_num = 4
+    mc = c // p_num
+    y = x.reshape(n, p_num, mc, h, w)
+    y = softmax(y, axis=2)
+    acc_metrix = np.arange(mc, dtype=float).reshape(1, 1, mc, 1, 1)
+    y = (y * acc_metrix).sum(2)
+
+    return y
+
+
+def box_process(position):
+    grid_h, grid_w = position.shape[2:4]
+    col, row = np.meshgrid(np.arange(0, grid_w), np.arange(0, grid_h))
+    col = col.reshape(1, 1, grid_h, grid_w)
+    row = row.reshape(1, 1, grid_h, grid_w)
+    grid = np.concatenate((col, row), axis=1)
+    stride = np.array([IMG_SIZE[1] // grid_h, IMG_SIZE[0] // grid_w]).reshape(1, 2, 1, 1)
+
+    position = dfl(position)
+    box_xy = grid + 0.5 - position[:, 0:2, :, :]
+    box_xy2 = grid + 0.5 + position[:, 2:4, :, :]
+    xyxy = np.concatenate((box_xy * stride, box_xy2 * stride), axis=1)
+
+    return xyxy
+
+
+def post_process(input_data):
+    boxes, scores, classes_conf = [], [], []
+    defualt_branch = 3
+    pair_per_branch = len(input_data) // defualt_branch
+    # Python 忽略 score_sum 输出
+    for i in range(defualt_branch):
+        boxes.append(box_process(input_data[pair_per_branch * i]))
+        classes_conf.append(input_data[pair_per_branch * i + 1])
+        scores.append(np.ones_like(input_data[pair_per_branch * i + 1][:, :1, :, :], dtype=np.float32))
+
+    def sp_flatten(_in):
+        ch = _in.shape[1]
+        _in = _in.transpose(0, 2, 3, 1)
+        return _in.reshape(-1, ch)
+
+    boxes = [sp_flatten(_v) for _v in boxes]
+    classes_conf = [sp_flatten(_v) for _v in classes_conf]
+    scores = [sp_flatten(_v) for _v in scores]
+
+    boxes = np.concatenate(boxes)
+    classes_conf = np.concatenate(classes_conf)
+    scores = np.concatenate(scores)
+
+    # filter according to threshold
+    boxes, classes, scores = filter_boxes(boxes, scores, classes_conf)
+
+    # nms
+    nboxes, nclasses, nscores = [], [], []
+    for c in set(classes):
+        inds = np.where(classes == c)
+        b = boxes[inds]
+        c = classes[inds]
+        s = scores[inds]
+        keep = nms_boxes(b, s)
+
+        if len(keep) != 0:
+            nboxes.append(b[keep])
+            nclasses.append(c[keep])
+            nscores.append(s[keep])
+
+    if not nclasses and not nscores:
+        return None, None, None
+
+    boxes = np.concatenate(nboxes)
+    classes = np.concatenate(nclasses)
+    scores = np.concatenate(nscores)
+
+    return boxes, classes, scores
+
+
+def draw(image, boxes, scores, classes):
+    image_shape = image.shape
+    color = [(0, 255, 0), (0, 0, 255), (147, 20, 255)]
+    classes_set = list(set(classes))
+    for boxe, score, classe in zip(boxes, scores, classes):
+        classe_index = classes_set.index(classe)
+        left, top, right, bottom = [int(_b) for _b in boxe]
+        #print("%s @ (%d %d %d %d) %.3f" % (CLASSES[classe], left, top, right, bottom, score))
+        cv2.rectangle(image, (left, top), (right, bottom), color[classe_index % 3], 2)
+        if top < 30:
+            top = 30
+        cv2.putText(image, '{0} {1:.2f}'.format(CLASSES[classe], score),
+                    (left + 6, top - 6), cv2.FONT_HERSHEY_SIMPLEX, 0.6, color[classe_index % 3], 2)
+
+
+
+def img_check(path):
+    img_type = ['.jpg', '.jpeg', '.png', '.bmp']
+    for _type in img_type:
+        if path.endswith(_type) or path.endswith(_type.upper()):
+            return True
+    return False
+
+
+if __name__ == '__main__':
+    current_directory = os.getcwd()
+    print("Current Directory:", current_directory)
+
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    print("base_dir:", base_dir)
+
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    # # basic params
+    # parser.add_argument('--model_path', type=str, default='./model/yolov8n.rknn', help='model path, could be .pt or .rknn file')
+    parser.add_argument('--model_path', type=str, default='best5n.rknn', help='model path, could be .pt or .rknn file')
+    parser.add_argument('--target', type=str, default='rk3588', help='target RKNPU platform')
+    parser.add_argument('--video_path', type=str, default='video/009.mp4', help='video path for inference')
+    
+
+    args = parser.parse_args()
+    print(vars(args))
+
+
+
+
+    # init model
+    rknn_lite = RKNNLite()
+
+    print('--> Load RKNN model')
+    model_path = os.path.join(base_dir, args.model_path)
+    ret = rknn_lite.load_rknn(model_path)
+    if ret != 0:
+        print('Load RKNN model failed')
+        exit(ret)
+    print('done')
+
+    print('--> Init runtime environment')
+    if args.target == 'rk3588':
+        ret = rknn_lite.init_runtime(core_mask=RKNNLite.NPU_CORE_0)
+    else:
+        ret = rknn_lite.init_runtime()
+
+    if ret != 0:
+        print('Init runtime environment failed')
+        exit(ret)
+    print('done')
+
+    video_path = os.path.join(base_dir, args.video_path)
+    cap = cv2.VideoCapture(video_path)  # For Video
+
+    # Get the width and height of the video
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    
+
+    co_helper = COCO_test_helper(enable_letter_box=True)
+
+    frame_count = 0
+    start_time = time.time()
+
+    while True:
+        success, img_src = cap.read()
+        if not success:
+            print("End of video or error reading frame.")
+            break  # Exit the loop if the video ends or there is an error
+            # print("Err read frame")
+            # continue
+
+        frame_count += 1
+        
+        # Calculate and print the FPS every 30 frames
+        if frame_count % 30 == 0:
+            elapsed_time = time.time() - start_time
+            fps = int(frame_count / elapsed_time)
+            print(f"FPS: {fps:.2f}")
+
+        # if frame_count % 2 == 0:
+        if frame_count > -1:
+            # Get the width and height of the image
+            height, width = img_src.shape[:2]
+            
+
+            # print(img_src)
+
+
+            # img_src = cv2.imread(img_path)
+            if img_src is None:
+                continue
+
+            # Due to rga init with (0,0,0), we using pad_color (0,0,0) instead of (114, 114, 114)
+            pad_color = (0, 0, 0)
+            img = co_helper.letter_box(im=img_src.copy(), new_shape=(IMG_SIZE[1], IMG_SIZE[0]), pad_color=(0, 0, 0))
+            height, width = img.shape[:2]
+            
+
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            img = np.expand_dims(img, 0)
+
+            input_data = img
+            #print('--> Running model ')
+            outputs = rknn_lite.inference(inputs=[input_data])
+            boxes, classes, scores = post_process(outputs)
+        
+            img_p = img_src.copy()
+            if boxes is not None:
+                draw(img_p, co_helper.get_real_box(boxes), scores, classes)
+
+            if fps is not None:
+                cv2.putText(img_p, 'fps={0} '.format(fps), (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+
+            img_shape = img_p.shape
+            cv2.imshow('Image', img_p)
+            cv2.waitKey(1)
+
+    cap.release()  # Release the video capture object
+    cv2.destroyAllWindows()  # Close all OpenCV windows
